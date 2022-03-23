dd96319561a00e0cd32f413fc774a506af2053ac
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-23 00:09:57 -0400

    pulling multiple from db to react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,15 @@ import React, { useEffect, useState, createElement } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  const [form, setForm] = useState({title: "default", price: "default", description: "default"})
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
   const [dbtest, setDbtest] = useState({ assignment: "none", port: 0 });
+  const [form, setForm] = useState({
+    title: "default",
+    price: "default",
+    description: "default",
+  });
+  const [fromDb, setFromDb] = useState([
+    { title: "a", price: 0, description: "b" },
+  ]);
 
   useEffect(() => {
     p("useEffect Running");
@@ -27,7 +31,8 @@ function App() {
     axios
       .get("http://localhost:9000/api/pm/")
       .then((res) => {
-        console.log(res);
+        // console.log(res.data);
+        setFromDb(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -48,6 +53,17 @@ function App() {
     p(event.target.value);
   };
 
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+
+    p(event.target.value);
+    const newState = {
+      ...form,
+      [event.target.name]: event.target.value,
+    };
+    setForm(newState);
+  };
+
   return (
     <div className="App">
       <h1>Assignment Product Manager</h1>
@@ -55,15 +71,15 @@ function App() {
       <form onSubmit={onSubmitHandler}>
         <div className="box">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" />
+          <input type="text" name="title" onChange={onChangeHandler} />
         </div>
         <div className="box">
-          <label htmlFor="title">Price</label>
-          <input type="text" name="title" />
+          <label htmlFor="price">Price</label>
+          <input type="text" name="price" onChange={onChangeHandler} />
         </div>
         <div className="box">
-          <label htmlFor="title">Description</label>
-          <input type="text" name="title" />
+          <label htmlFor="description">Description</label>
+          <input type="text" name="description" onChange={onChangeHandler} />
         </div>
         <button className="btn btn-primary mx-4" onClick={onClickHandler}>
           Create PM
@@ -72,6 +88,29 @@ function App() {
       <div className="box">
         <p>Assignment: {dbtest.assignment}</p>
         <p>Port: {dbtest.port}</p>
+      </div>
+      <div className="box">
+        <p>Title: {form.title}</p>
+        <p>Price: {form.price}</p>
+        <p>Description: {form.description}</p>
+      </div>
+      <div className="box">
+        {fromDb.map((item, i) => {
+          console.log(`function run ${i}, item: ${item.title}`);
+          return (
+            <div>
+              <p name={item.title} key={i}>
+                {item.title}
+              </p>
+              <p name={item.price} key={i}>
+                {item.price}
+              </p>
+              <p name={item.description} key={i}>
+                {item.description}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
